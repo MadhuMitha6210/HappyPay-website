725090fe9d313b25fc9354ca455319bdddd7af17
MadhuMitha6210 <madhumitha3x@outlook.com>
2026-06-14 21:20:29 +0530

    \

--- src/components/Homescreen.jsx
+++ src/components/Homescreen.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import Navbar from "./Navbar";
-import homescreenpage1continutionimg from "../assets/homescreenpage1continutionimg.png";
+import homescreenpage1continutionimg from "../assets/homescreenpage1continutionimg.jpeg";
 const Home = () => {
     return (
 
-        <div className="text-black ">
+        <div className="text-black min-h-screen bg-gradient-to-r from-[#0491E9] to-[#436C86] ">
             <h1>  </h1>
 <Navbar />
 
-                     <div className=" bg-green-600 min-h-screen w-full">
-            <div className="text-black :text font-extrabold text-5xl pt-15 pl-30 pr-30">
-                <h1> SEND AND SPEND<br/> MONEY WORLDWISE </h1>
-            </div>
+            <div className=" min-h-screen w-full">
+                <div className="text-black :text font-extrabold md:text-8xl pt-15 text-white ">
+                    <h1> SEND AND SPEND<br/> MONEY WORLDWISE </h1>
+                </div>
                 <br/>
-                         <br/>
-                <div><h6>Save on hidden conversion fees when you use your <br/>Wise Travel card or send money abroad.</h6></div>
-<br/>
- <br/>                        <button className="bg-black : rounded-2xl : text-green-700"> <pre>  sign up in minutes  </pre>   </button>
+                <br/>
+                <div className="text-black :text font-semibold text-white "><p className="text-2xl">Save on hidden conversion fees when you
+                    use your <br/>Wise Travel card or send money abroad.</p></div>
+                <br/>
+                <br/>
 
-                         <img
-                             src= {homescreenpage1continutionimg}
-                             alt="continuation"
-                             className="absolute left-2/4 -translate-x-1/2 bottom-[-30vh] h-[55vh] object-cover pointer-events-none"
-                         />
-                      </div>
+                <button className="bg-white px-8 py-3 rounded-full font-bold text-xl ">
+                <span className="inline-block bg-gradient-to-r from-[#0491E9] to-[#436C86] bg-clip-text text-transparent">
+                        SIGN UP IN MINUTES
+                </span>
+                </button>
+
+
+                <img
+                    src={homescreenpage1continutionimg}
+                    alt="continuation"
+                    className="absolute left-2/4 -translate-x-1/2 bottom-[-65vh] h-[85vh] object-cover pointer-events-none "
+                />
+
+            </div>
         </div>
     );
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import logo from "../assets/happylogonav.png"
 const Navbar = () => {
     return (
-        <div className='flex flex-row gap-175'>
-            <div className='flex flex-row gap-6'>
-                <img src={logo} className=" w-25 h-10 pt-10px" alt='logo' />
-<p>personal</p>
-<p>platform</p>
-            <p>bussiness</p>
+        <div className='flex flex-row gap-175 mt-[10px]  mb-[10px] bg-white '>
+            <div className='flex flex-row gap-6 text-lg'>
+                <img src={logo} className=" w-30 h-15" alt='logo' />
+<p>Personal</p>
+<p>Platform</p>
+            <p>Bussiness</p>
             </div>
             <div className='flex flex-row gap-6'>
                 <p>EN</p>
